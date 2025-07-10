201f798822e162d0f359e90aa5f3741e10d9b754
Lucas Ortis <lucasortis01@gmail.com>
2025-07-10 14:31:14 -0300

    feat: Add Terraform helper script for environment-specific deployments

---
+++ terraform-helper.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# This script runs Terraform commands for a specified environment.
+# Usage: ./run-terraform.sh [-e=env|--env=env] [init|plan|apply]  
+
+rm -rf .terraform
+
+set -e
+
+ENV="dev"  # Default environment
+for arg in "$@"; do
+  case $arg in
+    -e=*|--env=*) ENV="${arg#*=}"; shift ;;
+  esac
+done
+
+BASE="./environments/$ENV"
+if [[ ! -d "$BASE" ]]; then
+  echo "Environment '$ENV' does not exist."
+  exit 1
+fi
+
+echo "Running Terraform $* for '$ENV'"
+
+echo "Running 'terraform init' for '$ENV'"
+  terraform init \
+    -backend-config="$BASE/terraform.s3.tfbackend" \
+    -upgrade \
+    -reconfigure
+
+if [ "$1" == "plan" ]; then
+  echo "Running 'terraform plan' for '$ENV'"
+  terraform plan -var-file="$BASE/terraform.tfvars" -out=tfplan
+  exit 0
+fi
+
+if [ "$1" == "apply" ]; then
+  echo "Running 'terraform apply' for '$ENV'"
+  terraform apply tfplan
+  exit 0
+fi
+
+if [ "$1" == "destroy" ]; then
+  echo "Running 'terraform destroy' for '$ENV'"
+  terraform destroy -var-file="$BASE/terraform.tfvars"
+  exit 0
+fi
+
+  
+  
